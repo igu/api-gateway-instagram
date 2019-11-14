564351ce8d35bac81ff3da81eac4829fcbc7aa21
Igor Alisson <igoralissoncn@gmail.com>
2019-11-14 14:43:43 -0300

    Implementando metodo de store e listagem da API de posts

--- src/controllers/PostController.js
+++ src/controllers/PostController.js
@@ -2,10 +2,22 @@ const Post = require('../models/Post');
 
 module.exports = {
     async index(req, res, next) {
-
+        const posts = await Post.find().sort('-createdAt');
+        return res.json(posts);
     },
 
     async store(req, res, next) {
-        return res.json({ ok: true });
+        const { author, place, description, hashtags } = req.body;
+        const { filename: image } = req.file;
+
+        const post = await Post.create({
+            author,
+            place,
+            description,
+            hashtags,
+            image,
+        });
+
+        return res.json(post);
     }
 };
