d189547d500e2c398a39eae26ce4ce2b9eb356a9
Igor Alisson <igoralissoncn@gmail.com>
2019-11-19 18:47:01 -0300

    Removendo importacoes desnecessarias do arquivo index de posts

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const path = require('path');
 
 const app = express();
 mongoose.connect('mongodb+srv://igu:igorigor@cluster0-ou3ye.mongodb.net/test?retryWrites=true&w=majority', {
     useNewUrlParser: true,
 });
 
-app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')));
-
 app.use(require('./routes'));
 app.listen(3001);
+console.log('API DE CLONE DO INSTAGRAM OUVINDO NA PORTA 3001');
