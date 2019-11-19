0eff6c406faf929250972876fb7e0fdd7d20da43
Igor Alisson <igoralissoncn@gmail.com>
2019-11-19 15:57:29 -0300

    Criando funcao para deletar post no controlador

--- src/controllers/PostController.js
+++ src/controllers/PostController.js
@@ -13,21 +13,32 @@ module.exports = {
         const { author, place, description, hashtags } = req.body;
         const { filename: image } = req.file;
 
+        const [name] = image.split('.');
+        const fileName = `${name}.jpg`;
+
+
         await sharp(req.file.path)
             .resize(500)
             .jpeg( { quality: 70 } )
             .toFile(
-                path.resolve(req.file.destination, 'resized', image)
+                path.resolve(req.file.destination, 'resized', fileName)
             )
+
+        fs.unlinkSync(req.file.path);
 
         const post = await Post.create({
             author,
             place,
             description,
             hashtags,
-            image,
+            image: fileName,
         });
 
         return res.json(post);
-    }
+    },
+
+    async delete(req, res, next) {
+        const posts = await Post.deleteOne( { _id: `${req.params.id}` } );
+        return res.json({ delete: 'ok' });
+    },
 };
