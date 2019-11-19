9a8efc9c3b187b7c539660a1ec69a990442e3bfb
Igor Alisson <igoralissoncn@gmail.com>
2019-11-19 15:56:55 -0300

    Corrigindo problema com resized de imagens

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const app = express();
-mongoose.connect('mongodb+srv://igu:igorigor@cluster0-ahzmr.mongodb.net/test?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://igu:igorigor@cluster0-ou3ye.mongodb.net/test?retryWrites=true&w=majority', {
     useNewUrlParser: true,
 });
+
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')));
 
 app.use(require('./routes'));
 app.listen(3001);
