94be40679043fdb533e19bf85ed65f286d68a0e4
Igor Alisson <igoralissoncn@gmail.com>
2019-11-19 22:16:03 -0300

    Deletando arquivos desnecessarios ao contexto

--- uploads/index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-
-app.use('/files', express.static(path.resolve(__dirname, 'resized')));
-app.listen(3002);
-console.log('API DE FILES OUVINDO NA PORTA 3002');
