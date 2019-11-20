abb22b1bcb965f93692bef5c8b24dabac6d93d34
Igor Alisson <igoralissoncn@gmail.com>
2019-11-19 22:13:57 -0300

    Alterando as rotas do api-gateway

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const multer = require('multer');
-const uploadConfig = require('./config/upload');
+const uploadConfig = require('./api-post-insta/config/upload');
 
-const PostController = require('./controllers/PostController');
-const LikeController = require('./controllers/LikeController');
+const PostController = require('./api-post-insta/controllers/PostController');
+const LikeController = require('./api-like-insta/controllers/LikeController');
 
 const routes = new express.Router();
 const upload = multer(uploadConfig);
 
 routes.post('/posts', upload.single('image'), PostController.store);
 routes.get('/posts', PostController.index);
-
 routes.delete('/posts/:id', PostController.delete);
+
 routes.post('/posts/:id/like', LikeController.store);
+routes.post('/posts/:id/deslike', LikeController.update);
 
 module.exports = routes;
