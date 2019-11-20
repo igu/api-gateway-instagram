fb8fe71818e1f1e6349dbdfe6ee94b39b1b35717
Igor Alisson <igoralissoncn@gmail.com>
2019-11-19 22:33:19 -0300

    Movendo arquivos

--- api-gateway/URLs.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    INSTAGRAM_POSTS_API_URL_CREATE: 'http://localhost:3001/posts',
-    INSTAGRAM_POSTS_API_URL_LIST: 'http://localhost:3001/posts',
-    INSTAGRAM_POSTS_API_URL_DELETE: 'http://localhost:3001/posts/:id',
-
-    INSTAGRAM_POSTS_API_URL_LIKE: 'http://localhost:3002/posts/:id/like',
-    INSTAGRAM_POSTS_API_URL_DESLIKE: 'http://localhost:3001/3002/:id/deslike'
-};
